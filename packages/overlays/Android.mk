31ac0f28350bce9ff2a6fdf2f5fd541a2355a100
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-07-24 17:13:07 +0530

    overlays: Add an OxygenOS icon pack
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -88,6 +88,12 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
+<<<<<<< HEAD
+=======
+	NavigationBarMode2ButtonOverlay \
+	IconPackOOSAndroidOverlay \
+	IconPackOOSSystemUIOverlay \
+>>>>>>> a4843e005a19... overlays: Add an OxygenOS icon pack
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
